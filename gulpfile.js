d4eff4d8bd44d39442879a9fb70d4b2d677533cf
oluyoung <kanyinsola_olugbake@yahoo.co.uk>
2018-08-01 22:26:01 +0100

    Initial commit: added header, footer; added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+// var del = require('del');
+
+var paths = {
+  scripts: 'src/js/*',
+  styles: 'src/css/*',
+  images: 'src/images/*'
+}
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/javascripts'));
+});
+
+// Copy all static images
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    // Pass in options to the task
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('public/img'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'scripts', 'images']);
