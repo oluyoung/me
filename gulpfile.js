06fa81e850148a3c54b410c10afd36e7dcc661dd
oluyoung <kanyinsola_olugbake@yahoo.co.uk>
2018-08-02 21:26:25 +0100

    pretty much done

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,24 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var cleanCSS = require('gulp-clean-css');
 // var del = require('del');
 
 var paths = {
-  scripts: 'src/js/*',
-  styles: 'src/css/*',
-  images: 'src/images/*'
+  scripts: 'src/js/*.js',
+  styles: 'src/css/*.css',
+  images: 'src/images/*',
+  pugs: 'src/pug/*.pug',
+  sass: 'src/sass/*.scss'
 }
 
-gulp.task('scripts', function() {
+gulp.task('uglify-js', function() {
   return gulp.src(paths.scripts)
     .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(concat('scripts.min.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('public/javascripts'));
 });
@@ -28,11 +33,37 @@ gulp.task('images', function() {
     .pipe(gulp.dest('public/img'));
 });
 
+
+gulp.task('pugs', function buildHTML() {
+  return gulp.src(paths.pugs)
+  .pipe(pug({ // Your options in here.
+  }))
+  .pipe(gulp.dest('public/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.sass)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('uglify-css', function(){
+  return gulp.src(paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('styles.min.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scripts, ['uglify-js']);
   gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.pugs, ['pugs']);
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.styles, ['uglify-css']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'images']);
+gulp.task('default', ['watch', 'uglify-js', 'images', 'pugs', 'sass', 'uglify-css']);
